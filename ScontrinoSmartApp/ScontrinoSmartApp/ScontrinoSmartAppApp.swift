18567942e19391e82f944ce4a5b06a2b40c9fd20
Mahdi Miri <mahdimiri7@gmail.com>
2025-11-10 11:57:11 +0100

    .

---
+++ ScontrinoSmartApp/ScontrinoSmartApp/ScontrinoSmartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScontrinoSmartAppApp.swift
+//  ScontrinoSmartApp
+//
+//  Created by Mahdi Miri on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScontrinoSmartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
