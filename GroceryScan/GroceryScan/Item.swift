bafd770d7c7da3f020732b448313660401ed702f
Mahdi Miri <mahdimiri7@gmail.com>
2025-11-13 11:30:46 +0100

    .

---
+++ GroceryScan/GroceryScan/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GroceryScan
+//
+//  Created by Mahdi Miri on 13/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
